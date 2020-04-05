931e9085f12471bf167bef421eba3ca70abd946b
Stephen Limcangco <stephenlimcangco@gmail.com>
2020-04-05 23:53:02 +1000

    check if input is a number

--- calc/main.swift
+++ calc/main.swift
@@ -11,6 +11,25 @@ import Foundation
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
+var string: [String] = args
+
+var numberArray: [Int] = []
+var operatorArray: [String] = []
+
+for i in 0..<string.count {
+    if (isNumber(index: i)){
+        numberArray.append(Int(string[i])!)
+    }
+}
+
+ func isNumber(index: Int) -> Bool {
+    if ((Int(string[index])) != nil){
+        return true
+    } else {
+        return false
+    }
+}
+
 // Retrieve User Input
 //let no1 = args[0]; // Sample Code Only! Update Required!
 //let operator = args[1]; // Sample Code Only! Update Required!
